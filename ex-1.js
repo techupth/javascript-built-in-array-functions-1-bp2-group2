a654775ff72ff08e3f0c971e875c819d2107ff82
T <itthi91@gmail.com>
2023-12-06 07:52:02 +0700

    feat(assignment): js-bult-in-array-func-01

--- ex-1.js
+++ ex-1.js
@@ -2,7 +2,14 @@ let words = ["hello", "world"];
 
 function getWordLengths(words) {
   // Start coding here
+  let result = [];
+  for (let i = 0; i < words.length; i++) {
+    result.push(words[i].length)
+  }
+  return result;
 }
+
+let wordArr = words.map(getWordLengths)
 
 const result = getWordLengths(words);
 
--- ex-2.js
+++ ex-2.js
@@ -2,7 +2,17 @@ let words = ["apple", "cat", "dog", "elephant"];
 
 function get5CharWords(words) {
   // Start coding here
+  let result = [];
+  for (let i = 0; i < words.length; i++) {
+    if (words[i].length >= 5) {
+      result.push(words[i])
+    }
+  }
+  return result;
 }
+
+
+let filteredWords = words.filter(get5CharWords)
 
 const result = get5CharWords(words);
 
